786fdc883ad1f9a5ab33a15e2bbfa907119c86f3
thomas-waite <thomas.waite4@gmail.com>
2023-04-07 14:22:15 +0100

    feat: remove node_modules from git tracking

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/web3warrior/Documents/code/gnars-docs/gnars-docs-git/gnars-2/gnars-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/web3warrior/Documents/code/gnars-docs/gnars-docs-git/gnars-2/gnars-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/web3warrior/Documents/code/gnars-docs/gnars-docs-git/gnars-2/gnars-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/web3warrior/Documents/code/gnars-docs/gnars-docs-git/gnars-2/gnars-docs/src/css/custom.css'),
-];
