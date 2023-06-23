85017ddb8d7e972a87b05ab4fb4e1d232e69bfc2
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-06-23 12:22:46 -0300

    ai search

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+
 const FeatureList = [
   {
     title: 'WTF is a DAO?',
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,19 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import styles from './index.module.css';
+import { MendableSearchBar } from '@mendable/search';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Nerd it up ! 
+          <Link className="button button--secondary button--lg" to="/docs/intro">
+            Nerd it up!
           </Link>
         </div>
       </div>
@@ -27,16 +25,14 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
+        <MendableSearchBar anon_key="c08b6d38-b2b2-4ee9-abe5-34753f2fa9c2" style={{ margin: '2rem auto', maxWidth: '800px' }} />
         <HomepageFeatures />
       </main>
     </Layout>
   );
 }
-
