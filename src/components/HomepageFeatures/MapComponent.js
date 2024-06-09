019d4279a5eb87626a6bb8ddc66fedcd48b62b4c
Vlad Nikolaev <web3warrior@macbook.local>
2024-06-08 23:42:32 -0300

    added more noggles rails

--- src/components/HomepageFeatures/MapComponent.js
+++ src/components/HomepageFeatures/MapComponent.js
@@ -7,11 +7,13 @@ const MapComponent = () => {
 
     const position = [0, 0]; // Center of the map
     const locations = [
-        { position: [-22.903044816157887, -43.17337963607664], label: 'Praca XV', imageUrl: "https://i.ibb.co/NsDT8zr/ezgif-com-video-to-gif.gif" },
+        { position: [-22.903044816157887, -43.17337963607664], label: 'Praca XV', imageUrl: "/rocknogles.gif" },
         { position: [33.81427083205093, -118.21369178292444], label: 'Silverado', imageUrl: "" },
         { position: [41.965330396404994, -87.6638363963253], label: 'Chicago', imageUrl: "" },
         { position: [-30.017866183250845, -51.17985537072372], label: 'Iapi', imageUrl: "" },
         { position: [9.0820, 8.6753], label: 'Africa', imageUrl: "" },
+        { position: [-23.4990518351234, -46.624191393782525], label: 'Sopa de Letras', imageUrl: "/sopadeletras.png" },
+        { position: [-20.24901180535837, -42.029355475124554], label: 'Manhuaçu', imageUrl: "/manhuacurail.png" },
     ];
 
     return MapContainer ? (
