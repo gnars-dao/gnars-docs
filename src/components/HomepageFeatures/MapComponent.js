e42b40279659c8251ddc6e00785c74536a0f4e11
Vlad 2 <116202536+sktbrd@users.noreply.github.com>
2024-07-18 19:57:18 -0300

    Update MapComponent.js
    added davesbachinki ramp in the map, we need a new icon for ramps

--- src/components/HomepageFeatures/MapComponent.js
+++ src/components/HomepageFeatures/MapComponent.js
@@ -15,6 +15,7 @@ const MapComponent = () => {
         { position: [-23.4990518351234, -46.624191393782525], label: 'Sopa de Letras', imageUrl: "/sopadeletras.png" },
         { position: [-20.24901180535837, -42.029355475124554], label: 'Manhuaçu', imageUrl: "/manhuacurail.png" },
         { position: [42.737274371776024, 140.9109422458354], label: 'Rusutsu Resort', imageUrl: "/rutsujpg.jpg" },
+        { position: [33.71824554962641, -117.84727040288683], label: 'OC Ramp', imageUrl: "" },
     ];
 
     return MapContainer ? (
